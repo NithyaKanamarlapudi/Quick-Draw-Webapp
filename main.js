564b83e91313c69ff36ac28aac02997d261080e0
NithyaKanamarlapudi <76540341+NithyaKanamarlapudi@users.noreply.github.com>
2021-11-27 16:15:53 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var drawn_sketch;
+var sketch;
+var score=0;
+var answer_holder;
+var timer_counter=0;
+var timer_check;
+var answer_holder;
 function preload(){
 
 }
@@ -6,6 +13,31 @@ function setup(){
         canvas.center();
         background("white");
 }   
-    function clearCanvas(){
+    function updateCanvas(){
         background("white");
+        var random_number= Math.floor((Math.random()*quick_draw_data_set.length)+1);
+        console.log(quick_draw_data_set[random_number]);
+        sketch=quick_draw_data_set[random_number];
+        document.getElementById('sketch_to_be_drawn').innerHTML = 'Sketch to be Drawn: '+ sketch;
+    }
+    function draw(){
+    check_sketch()
+        if (drawn_sketch==sketch){
+            answer_holder=set;
+            score=score+1;
+            document.getElementById("score").innerHTML='Score: '+score;
+        }
+    }
+    function check_sketch(){
+        timer_counter=timer_counter+1;
+        document.getElementById('timer').innerHTML= 'Timer: '+timer_counter;
+        if(timer_counter>400){
+            timer_counter=0;
+            timer_check="completed";
+        }
+        if(timer_check=="complete" || answer_holder=="set"){
+            timer_check="";
+            answer_holder="";
+            updateCanvas()
+        }
     }
