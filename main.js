53d19f36bab6cf67bea2e990bab54a42fd060855
NithyaKanamarlapudi <76540341+NithyaKanamarlapudi@users.noreply.github.com>
2022-02-02 18:14:17 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var drawn_sketch;
+var drawn_sketch="";
 var sketch;
 var score=0;
-var answer_holder;
+var answer_holder="";
 var timer_counter=0;
-var timer_check;
-var answer_holder;
+var timer_check="";
 function preload(){
-
+classifier=ml5.imageClassifier('DoodleNet')
 }
 function setup(){
     canvas=createCanvas(280,280);
         canvas.center();
         background("white");
+        canvas.mouseReleased(classifyCanvas);
 }   
     function updateCanvas(){
         background("white");
@@ -21,13 +21,32 @@ function setup(){
         document.getElementById('sketch_to_be_drawn').innerHTML = 'Sketch to be Drawn: '+ sketch;
     }
     function draw(){
+        strokeWeight(10);
+        stroke(0);
+        if(mouseIsPressed){
+            line(pmouseX, pmouseY, mouseX, mouseY);
+        }
     check_sketch()
         if (drawn_sketch==sketch){
-            answer_holder=set;
+            answer_holder="set";
             score=score+1;
             document.getElementById("score").innerHTML='Score: '+score;
         }
     }
+    function classifyCanvas(){
+        classifier.classify(canvas, gotResult);
+    }
+
+    function gotResult(error, results){
+        if (error){
+            console.error();
+        }
+        console.log(results);
+        drawn_sketch=results[0].label;
+        document.getElementById("label").innerHTML="label"+drawn_sketch;
+        document.getElementById("confidence").innerHTML="confidence"+Math.round(result[0].confidence*100)+'%';
+        }
+
     function check_sketch(){
         timer_counter=timer_counter+1;
         document.getElementById('timer').innerHTML= 'Timer: '+timer_counter;
@@ -35,7 +54,7 @@ function setup(){
             timer_counter=0;
             timer_check="completed";
         }
-        if(timer_check=="complete" || answer_holder=="set"){
+        if(timer_check=="completed" || answer_holder=="set"){
             timer_check="";
             answer_holder="";
             updateCanvas()
